1d13ebdf99b8af64d171d73b00e3b1b4756728c7
Krapaud <mur.mickael@gmail.com>
2025-08-06 11:08:54 +0200

    Implement bubble sort algorithm and add swap function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,20 @@
 #include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * swap - Swaps two elements in an array
+ * @array: The array containing the elements to swap
+ * @i: Index of the first element
+ * @j: Index of the second element
+ */
+void swap(int *array, int i, int j)
+{
+	int temp = array[i];
+
+	array[i] = array[j];
+	array[j] = temp;
+}
 /**
  * bubble_sort - Sorts an array of integers in ascending order using
  *               the Bubble sort algorithm
@@ -8,5 +23,17 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    /* TODO: Implement bubble sort algorithm */
+	int i, j;
+
+	int n = size;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				swap(array, j, j + 1);
+				print_array(array, size);
+			}
+	}
 }
